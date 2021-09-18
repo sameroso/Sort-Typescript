9dc6c3d0a398e7e7429d8a24c4c4e91159dfba86
Samer Kayali <samerkkr@gmail.com>
2021-09-17 22:10:45 -0300

    bad solution

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        // If collection is an array of number
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
         }
+      }
+
+      // If collection is a string
+      if (typeof this.collection === "string") {
+      
       }
     }
   }
