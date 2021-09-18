2b1ddff9a978ba2f0d0744174a4e3709229bdf4e
Samer Kayali <samerkkr@gmail.com>
2021-09-17 22:55:12 -0300

    refactoring and adding more sorting classes

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,14 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // If collection is an array of number
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-      }
+// const numbersCollection = new NumbersCollection([1, -1, 20, 49]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
 
-      // If collection is a string
-      if (typeof this.collection === "string") {
-      
-      }
-    }
-  }
-}
+const charactersCollection = new CharactersCollection("samer");
+const sorter = new Sorter(charactersCollection);
+sorter.sort()
 
-const sorter = new Sorter([4, 9, 2, -4, 7, 8]);
-sorter.sort();
-console.log(sorter.collection);
+console.log(charactersCollection.data);
