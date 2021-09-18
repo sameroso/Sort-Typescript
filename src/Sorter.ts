d43f0b4550f6e6c28c8cef65cce8a2fe98a620c2
Samer Kayali <samerkkr@gmail.com>
2021-09-17 22:55:12 -0300

    refactoring and adding more sorting classes

---
+++ src/Sorter.ts
@@ -0,0 +1,20 @@
+export interface Sortable {
+  swap(leftIndex: number, rightIndex: number): void;
+  compare(leftIndex: number, rightIndex: number): boolean;
+  length: number;
+}
+
+export class Sorter {
+  constructor(public collection: Sortable) {}
+
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection.compare(j, j + 1)) {
+          this.collection.swap(j, j + 1);
+        }
+      }
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,7 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-  sort(): void {
-    const { length } = this.collection;
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // If collection is an array of number
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-      }
-
-      // If collection is a string
-      if (typeof this.collection === "string") {
-      
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([4, 9, 2, -4, 7, 8]);
+const numbersCollection = new NumbersCollection([1, -1, 20, 49]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
